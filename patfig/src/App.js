80b5f2599e8cc61114be527ee3bf5f9ce0f51ed3
AntonHuggard <antonhuggard@gmail.com>
2023-09-01 11:53:16 +1000

    + react JS

---
+++ patfig/src/App.js
@@ -0,0 +1,25 @@
+import './main.css';
+
+function App() {
+  return (
+    <div className="App">
+          <header>
+              <h1>PatFig.</h1>
+              <h3>Patent drawings made easy</h3>
+          </header>
+
+          <div id="main-content">
+              <h3>Under construction</h3>
+          </div>
+
+          <footer>
+              <div id="huggards" onclick="window.location.href='https://antonhuggard.com'">
+                  website by Anton Huggard
+              </div>
+              &copy; Anton Huggard 2023. All rights reserved.
+          </footer>
+    </div>
+  );
+}
+
+export default App;
