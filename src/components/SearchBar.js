1fb28f06b9e88808f0e802e80fad496b23d1e36e
AntonHuggard <antonhuggard@gmail.com>
2023-09-25 21:26:52 +1300

    update CSS

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,9 +4,9 @@ class SearchBar extends Component {
       
     render() {
         return(
-            <form id='search-bar-wrapper' method='GET'>
-                <input type='text' name='q'/>
-                <button type='submit'>Search!</button>
+            <form id='search-bar-wrapper' method='GET' autocomplete="off">
+                <input type='text' name='q' placeholder='type here...' id='search-bar'/>
+                <button type='submit' id='search-btn'>Search</button>
             </form>
         )
     }
