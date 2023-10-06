144c5582113d29065c84e7f6b72e82895a9bac45
AntonHuggard <antonhuggard@gmail.com>
2023-10-06 16:29:07 +1300

    + magnifying glass icon for  searching on mobile

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,10 @@ class SearchBar extends Component {
         return(
             <form id='search-bar-wrapper' method='GET' autocomplete="off">
                 <input type='text' name='q' placeholder='type here...' id='search-bar'/>
-                <button type='submit' id='search-btn'>Search</button>
+                <button type='submit' id='search-btn'>
+                    <img src={window.location.origin + '/search-icon.png'} />
+                    <span>Search</span>
+                </button>
             </form>
         )
     }
